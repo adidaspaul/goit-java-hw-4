902fe51725489944867d0ec447f0898604d4ee07
Poltava <80519488+adidaspaul@users.noreply.github.com>
2021-09-30 05:05:37 -0700

    Create ControlPanel.java

---
+++ ControlPanel.java
@@ -0,0 +1,24 @@
+class ControlPanelTest {
+    public static void main(String[] args) {
+        new ControlPanel().control();
+    }
+}
+class Altimeter{
+  @Override
+  public String toString(){
+    return "Measure height\n";
+  }
+}
+class AirGauge{
+  @Override
+  public String toString(){
+    return "Measure air pressure";
+  }
+}
+class ControlPanel{
+  private Altimeter altimeter = new Altimeter();
+private AirGauge airGauge = new AirGauge();
+public void control(){
+  System.out.println(altimeter.toString() + airGauge.toString());
+  }
+}
